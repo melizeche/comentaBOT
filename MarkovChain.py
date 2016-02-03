b16f1ea14fc9498a0d6089ef2d2cc2cb030e01ee
Marce Elizeche <melizeche@gmail.com>
2016-02-03 16:28:53 -0300

    try fix error

--- MarkovChain.py
+++ MarkovChain.py
@@ -109,8 +109,9 @@ class MarkovChain(object):
         # I'm assuming seeds tend to be rather short.
         words = seed.split()
         if (words[-1],) not in self.db:
+            pass
             # The only possible way it won't work is if the last word is not known
-            raise StringContinuationImpossibleError('Could not continue string: '
+            #raise StringContinuationImpossibleError('Could not continue string: '
                                                     + seed)
         return self._accumulateWithSeed(words)
 
